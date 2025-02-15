94e9393bd8ee625488a58a7179b7de4d91979cd3
abdulaliarafat <abdulaliarafat@gmail.com>
2025-02-15 16:05:27 +0600

    math equqtion complete

---
+++ Js problem part-1/maths.js
@@ -0,0 +1,23 @@
+// const Math = {
+//     min: function min(num1, num2){
+//          return num2
+//     }
+// }
+// console.log(Math.min)
+const min = Math.min(11,33,556,77,70,534)
+console.log(min)
+const max = Math.max(11,33,556,77,70,534)
+console.log(max)
+console.log(Math.PI.toFixed(2))
+console.log(Math.abs(10-39))
+console.log(Math.round(3.49))
+console.log(Math.floor(4.99))
+console.log(Math.ceil(2.00000001))
+// function..........
+// dashomic er por asbe
+console.log(Math.random())
+// dushomic soho asbe (6) purno sonkka
+console.log(Math.random()*10)
+// sudu purno sunkka
+const math = Math.round(Math.random()*10)
+console.log(math)
