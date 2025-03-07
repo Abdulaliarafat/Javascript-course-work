e1842215ad4f404ee7f45e018d1fa954fbd9d5af
abdulaliarafat <abdulaliarafat@gmail.com>
2025-03-07 15:22:22 +0600

    more ES6

---
+++ ES6-advance/object.js
@@ -0,0 +1,21 @@
+const person={
+    name:'korim',
+    age:22,
+    friends:['basar','rohim','kuddus'],
+    1:'1000',
+    true:'dshjgfdheysgf',
+    details:{
+      job: 'yes',
+      isMarrid: true,
+      status: 'not found',
+      father:{
+         name:'hanif',
+         age:49,
+         work:'shopkeepar'
+      }
+    }
+}
+// it not mandetory give string in numder ....
+// console.log(person['1']) 
+// console.log(person['details']['father']['age'])
+// console.log(person.web?.father?.name)
