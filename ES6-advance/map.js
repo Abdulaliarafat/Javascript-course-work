6ff788da887c10e2142aaae3e1e2f1211628ebe6
abdulaliarafat <abdulaliarafat@gmail.com>
2025-03-07 16:30:13 +0600

    more ES6

---
+++ ES6-advance/map.js
@@ -0,0 +1,21 @@
+// const number=[1,2,3,4,5,65,77]
+// const temp=[];
+// for (let index = 0; index < number.length; index++) {
+//     const element = number[index];
+//     const sum=element+3;
+//    temp.push(sum)
+// }
+
+// console.log(temp)
+// const newArray = number.map(value => value+1);
+// console.log(newArray);
+// const number=[1,2,3,4,5,7,8,9,10]
+// const newNumber=number.map(element=>{
+//   return  element*element
+// })
+// console.log(newNumber)
+const friends=['basar','rohim','kuddus']
+const newFriends = friends.map((element,index) =>{
+    console.log(index)
+    console.log(element)    
+})
