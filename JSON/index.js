274eb20fcbe437a518ea5cfb208cdd83d163b368
abdulaliarafat <abdulaliarafat@gmail.com>
2025-03-11 14:13:24 +0600

    learning json in js load data in UI

---
+++ JSON/index.js
@@ -0,0 +1,16 @@
+const loadUser=()=>{
+   fetch('https://jsonplaceholder.typicode.com/users')
+   .then(reponse=>reponse.json())
+   .then(data=>{
+   showUser(data)
+   })
+}
+const showUser=(users)=>{
+    const userContainer=document.getElementById('users')
+    for(const user of users){
+      console.log(user)
+      const li=document.createElement('li')
+      li.innerText=(user.name)
+      userContainer.appendChild(li)
+    }
+}
